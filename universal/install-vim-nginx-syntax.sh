c664ffb07afd0c5a5fbf70fcd430ca29e7d3313d
JCloudYu <j.cloud.yu@gmail.com>
2017-11-01 02:36:53 +0800

    Add nginx syntax definition for vim installation logic

---
+++ universal/install-vim-nginx-syntax.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Start installing nginx's vim syntax file...";
+mkdir -p ~/.vim/syntax/
+cd ~/.vim/syntax/
+echo -n "    Fetching syntax file... ";
+curl -s https://raw.githubusercontent.com/JCloudYu/centos-scripts/master/nginx/nginx.vim > nginx.vim
+if [ $? -ne 0 ]; then
+	echo "Failed!" exit 1;
+else
+	echo "Success!";
+fi;
+
+echo "    Registering file name hook...";
+cat > ~/.vim/filetype.vim <<EOF
+au BufRead,BufNewFile nginx.conf,/etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif
+EOF
