bea4054951f0f433ecda08853831ec30915687c1
JCloudYu <j.cloud.yu@gmail.com>
2017-11-24 17:34:40 +0800

    1. Prevent environment without lsb_release package 2. Add gcc devtool installation logics 3. Add installation of nodejs 9.x environment

--- centos/centos-install.sh
+++ centos/centos-install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-CENTOS_VER=$(lsb_release -r | grep -oP "[0-9]+" | head -1);
+which lsb_release > /dev/null  2>&1;
+if [ $? -ne 0 ]; then
+	CENTOS_VER=$(cat /etc/centos-release | awk '{print $3}' | sed 's/\./\ /g' | awk '{print $1}');
+else
+	CENTOS_VER=$(lsb_release -r | grep -oP "[0-9]+" | head -1);
+fi;
 
 
 
@@ -9,7 +14,7 @@ if [ $? -ne 0 ]; then echo "Error installing epel-release package"; fi;
 
 
 
-echo "Installing tools... (wget vim unzip yum-utils python-devel python-pip start-stop-daemon)";
+echo "Installing tools... (wget vim unzip yum-utils python-devel python-pip start-stop-daemon screen)";
 yum -y install wget vim unzip yum-utils python-devel python-pip start-stop-daemon git java curl screen
 echo -e "set ts=4\nset nu\nset noai\n" >> /etc/vimrc
 
@@ -39,7 +44,6 @@ fi;
 
 
 
-
 echo -n "Install mongodb? [y/n] ";
 read DECISION;
 if [ "${DECISION}" == "y" ]; then
@@ -49,20 +53,37 @@ fi;
 
 
 
-
-
-echo -n "Install nodejs 8.x? [y/n] ";
+echo -n "Install nodejs? [y/n] ";
 read DECISION;
 if [ "${DECISION}" == "y" ]; then
-	curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -;
+	echo -n "Install nodejs 9.x? [y/n] ";
+	read DECISION;
+	if [ "${DECISION}" == "y" ]; then
+		curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -;
+	else
+		curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+	fi;
+
 	yum -y install nodejs;
 fi;
 
 
 
-echo -n "Install git from Wendisco? [y/n]";
+echo -n "Install git from Wendisco? [y/n] ";
 read DECISION;
-if [ "$DECISION" == ""y ]; then
+if [ "$DECISION" == "y" ]; then
 	wget https://raw.githubusercontent.com/JCloudYu/bash-scripts/master/centos/repos/wandisco-git.repo -O /etc/yum.repos.d/wandisco-git.repo
 	yum install -y git	
 fi
+
+
+if [ "${CENTOS_VER}" -le 6 ]; then
+	echo -n "Install devtool? [y/n] ";
+	read DECISION;
+	if [ "${DECISION}" == "y" ]; then
+		wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+		yum upgrade -y
+		yum install -y devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
+		scl enable devtoolset-2 bash
+	fi;
+fi;
